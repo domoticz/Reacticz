f32ae41915e49af9500d6ddadcea71f233c824e7
Tom Granger <tom@t0m.fr>
2017-02-11 00:33:14 +0100

    Fix #76

--- src/widgets/ThermostatWidget.js
+++ src/widgets/ThermostatWidget.js
@@ -1,9 +1,3 @@
-/*
-{"Battery":255,"RSSI":10,"description":"","dtype":"Heating","id":"92130","idx":131,"name":"thermostat","nvalue":0,"stype":"Zone","svalue1":"0.0","svalue2":"0.0","svalue3":"Auto","unit":1}
-{"Battery":255,"RSSI":12,"description":"","dtype":"Heating","id":"92130","idx":131,"name":"thermostat","nvalue":0,"stype":"Zone","svalue1":"20.5","unit":1}
-{"Battery":255,"RSSI":12,"dtype":"Thermostat","id":"0000001","idx":283,"name":"Thermostaat","nvalue":0,"stype":"SetPoint","svalue1":"17.00","unit":0}
-*/
-
 import React, { Component } from 'react';
 import JSONClientSingleton from '../util/JSONClientSingleton'
 import './ThermostatWidget.css';
@@ -14,6 +8,7 @@ class ThermostatWidget extends Component {
     super(props);
     this.json = new JSONClientSingleton();
     this.state = {
+      initiatedUpdate: false,
       targetValue: parseFloat(this.props.value, 10),
       throttleId: null
     };
@@ -33,6 +28,7 @@ class ThermostatWidget extends Component {
     }
     window.clearTimeout(this.state.throttleId);
     this.setState({
+      initiatedUpdate: true,
       targetValue:value,
       throttleId: window.setTimeout(() => this.emitSetpointUpdate(), 1000)
     });
@@ -46,11 +42,13 @@ class ThermostatWidget extends Component {
       nvalue: 0,
       svalue: this.state.targetValue
     };
-    this.json.get(message);
+    this.json.get(message, () => this.setState({initiatedUpdate: false}));
   }
 
   render() {
-    const updating = this.state.targetValue !==
+    const updating = this.state.initiatedUpdate ?
+        this.state.targetValue !== parseFloat(this.props.value, 10) : false
+    const displayValue = updating ? Number(this.state.targetValue).toFixed(1) :
         parseFloat(this.props.value, 10);
     const theme = this.props.theme;
     const style = theme ? {
@@ -67,8 +65,7 @@ class ThermostatWidget extends Component {
         <div className="controls">
           <button className="switch minus" style={buttonStyle}
                   onClick={this.decreaseSetpoint}>-</button>
-          <div className={updating ? 'blink' : ''}>
-            {Number(this.state.targetValue).toFixed(1)}</div>
+          <div className={updating ? 'blink' : ''}>{displayValue}</div>
           <button className="switch plus" style={buttonStyle}
                   onClick={this.increaseSetpoint}>+</button>
         </div>
