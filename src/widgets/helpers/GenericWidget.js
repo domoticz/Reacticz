45933fcdd718ca0e36b18c8db624ea1f5f592de2
Tom Granger <tom@t0m.fr>
2017-11-19 15:15:16 +0100

    Allow some HTML tagsvin text widgets. Allowed tags are: a, b, blockquote, br, caption, code, div, em, h3, h4, h5, h6, hr, i, img, li, nl, ol, p, pre, strike, strong, table, tbody, td, th, thead, tr, ul. Most attributes are removed (including 'style') to limit XSS vectors.

--- src/widgets/helpers/GenericWidget.js
+++ src/widgets/helpers/GenericWidget.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
+import sanitizeHtml from 'sanitize-html';
 import './GenericWidget.css';
 
 class GenericWidget extends Component {
 
+  sanitize(htmlString) {
+    console.log(sanitizeHtml.defaults.allowedTags.concat([ 'img' ]));
+    return {__html: sanitizeHtml(htmlString, {
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ])})};
+  }
+
   render() {
     const theme = this.props.theme;
     const style = theme ? {
-      background: this.props.readOnly ? theme.unlockedBackground : theme.background,
+      background: this.props.readOnly ?
+          theme.unlockedBackground : theme.background,
       color: theme.text,
       fill: this.props.isOn === undefined ? '' :
           (this.props.isOn ? theme.buttonOn : theme.buttonOff)
     } : {};
+    const safeValue1 = this.sanitize(this.props.value1);
+    const safeValue2 = this.props.value2 === undefined ?
+        null : this.sanitize(this.props.value2);
     return (
       <div className={'GenericWidget '+ this.props.class} style={style}>
         {this.props.icon && <svg><use xlinkHref={'#' + this.props.icon}/></svg>}
         {this.props.children}
         <section>
           <div>{this.props.label}</div>
-          <h2 className="value1">{this.props.value1}</h2>
-          {this.props.value2 !== undefined &&
-              <div className="value2">{this.props.value2}</div>}
+          <h2 className="value1" dangerouslySetInnerHTML={safeValue1}></h2>
+          {safeValue2 && <div className="value2"
+              dangerouslySetInnerHTML={safeValue2}></div>}
         </section>
       </div>
     );
