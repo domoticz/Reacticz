domoticz/Reacticz

commit 45933fcdd718ca0e36b18c8db624ea1f5f592de2

Allow some HTML tagsvin text widgets. Allowed tags are: a, b, blockquote, br, caption, code, div, em, h3, h4, h5, h6, hr, i, img, li, nl, ol, p, pre, strike, strong, table, tbody, td, th, thead, tr, ul. Most attributes are removed (including 'style') to limit XSS vectors.

src/widgets/helpers/GenericWidget.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
+import sanitizeHtml from 'sanitize-html';
 import './GenericWidget.css';
 
 class GenericWidget extends Component {
 
+  sanitize(htmlString) {
+    console.log(sanitizeHtml.defaults.allowedTags.concat([ 'img' ]));
+    return {__html: sanitizeHtml(htmlString, {
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ])})};
+  }
+
   render() {
     const theme = this.props.theme;
     const style = theme ? {
-      background: this.props.readOnly ? theme.unlockedBackground : theme.background,
+      background: this.props.readOnly ?
+          theme.unlockedBackground : theme.background,
       color: theme.text,
       fill: this.props.isOn === undefined ? '' :
           (this.props.isOn ? theme.buttonOn : theme.buttonOff)
     } : {};
+    const safeValue1 = this.sanitize(this.props.value1);
+    const safeValue2 = this.props.value2 === undefined ?
+        null : this.sanitize(this.props.value2);
     return (
       <div className={'GenericWidget '+ this.props.class} style={style}>
         {this.props.icon && <svg><use xlinkHref={'#' + this.props.icon}/></svg>}
         {this.props.children}
         <section>
           <div>{this.props.label}</div>
-          <h2 className="value1">{this.props.value1}</h2>
-          {this.props.value2 !== undefined &&
-              <div className="value2">{this.props.value2}</div>}
+          <h2 className="value1" dangerouslySetInnerHTML={safeValue1}></h2>
+          {safeValue2 && <div className="value2"
+              dangerouslySetInnerHTML={safeValue2}></div>}
         </section>
       </div>
     );

commit 3240fbc6098432cbe7bb1b7e05150d1ddfcf1a72

Fix weather/temp widgets broken by commit 45933fc

src/widgets/TemperatureWidget.js
@@ -10,7 +10,8 @@ class TemperatureWidget extends Component {
       return (
       <GenericWidget class="TemperatureWidget" icon="water-percent"
         isOn={this.props.humstat > 2}
-        value1={<div className="humonly">{this.props.humonly}</div>}
+        value1={this.props.humonly}
+        value1Class="humonly"
         value2={humidityStatus}
         {...this.props} />
       );
@@ -18,8 +19,10 @@ class TemperatureWidget extends Component {
       return (
       <GenericWidget class="TemperatureWidget" icon="thermometer-lines"
         isOn={this.props.temp > 19.9}
-        value1={<div className="temp">{this.props.temp}</div>}
-        value2={this.props.hum !== undefined && <div className="hum">{this.props.hum}</div>}
+        value1={this.props.temp}
+        value1Class="temp"
+        value2={this.props.hum}
+        value2Class="hum"
         {...this.props} />
       );
   }

src/widgets/WeatherInfoWidget.js
@@ -9,7 +9,8 @@ class WeatherInfoWidget extends Component {
       return (
       <GenericWidget class="WeatherInfoWidget" icon="eye"
         isOn={this.props.visibility > 0}
-        value1={<div className="visibility">{this.props.visibility}</div>}
+        value1={this.props.visibility}
+        value1Class="visibility"
         {...this.props} />
       );
     }
@@ -17,14 +18,16 @@ class WeatherInfoWidget extends Component {
       return (
       <GenericWidget class="WeatherInfoWidget" icon="weather-sunny"
         isOn={this.props.radiation > 0}
-        value1={<div className="radiation">{this.props.radiation}</div>}
+        value1={this.props.radiation}
+        value1Class="radiation"
         {...this.props} />
       );
     }
     return (
     <GenericWidget class="WeatherInfoWidget" icon="sunglasses"
       isOn={this.props.uv > 0}
-      value1={<div className="uv">{this.props.uv}</div>}
+      value1={this.props.uv}
+      value1Class="uv"
       {...this.props} />
     );
   }

src/widgets/helpers/GenericWidget.js
@@ -27,8 +27,11 @@ class GenericWidget extends Component {
         {this.props.children}
         <section>
           <div>{this.props.label}</div>
-          <h2 className="value1" dangerouslySetInnerHTML={safeValue1}></h2>
-          {safeValue2 && <div className="value2"
+          <h2 className={this.props.value1Class ?
+              this.props.value1Class : "value1"}
+              dangerouslySetInnerHTML={safeValue1}></h2>
+          {safeValue2 && <div className={this.props.value2Class ?
+              this.props.value2Class : "value2"}
               dangerouslySetInnerHTML={safeValue2}></div>}
         </section>
       </div>